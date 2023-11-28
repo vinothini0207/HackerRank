8cece6d87452f8436d5b4ac8f189d267a48527d5
vinothini0207 <vv0207490@gmail.com>
2023-11-28 13:18:51 +0530

    create JavaStdinAndStdout.java

---
+++ Introduction/JavaStdinAndStdout.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class JavaStdinAndStdout {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        // Complete this line
+        int b = scan.nextInt();
+        // Complete this line
+        int c = scan.nextInt();
+        System.out.println(a);
+         System.out.println(b);
+        // Complete this line
+         System.out.println(c);
+        // Complete this line
+    }
+}
