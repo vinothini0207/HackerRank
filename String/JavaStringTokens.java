97148f4287bdf6c32ead674a5ff08f59de79ff4d
vinothini0207 <vv0207490@gmail.com>
2023-11-29 18:46:43 +0530

    Modified

--- String/JavaStringTokens.java
+++ String/JavaStringTokens.java
@@ -1,30 +1,27 @@
 package HackerRank.String;
- import java.io.*;
+import java.io.*;
 import java.util.*;
-    
+
 public class JavaStringTokens {
-           public static void main(String[] args) {
-            Scanner scan = new Scanner(System.in);
-            String s = scan.nextLine().trim();
-            int num=0;
-            String[] words=s.split("[^A-Za-z0-9]+");
-            num=words.length;
-           if(s.length()==0){
-              System.out.println(0);
-            }
-            else{
-              System.out.println(num);
-              for(int i=0;i<num;i++){
-                System.out.println(words[i]);
-              }
-            }
-            // Write your code here.
-            scan.close();
-            
-            
-          
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine().trim();
+        System.out.println(s);
+        // Write your code here.
+        String[] strArray=s.split("[^A-Za-z0-9]+");
+        System.out.println(strArray.toString());
+        int num=strArray.length;
+        if(s.length()==0){
+            System.out.println(0);
         }
+        else{
+            System.out.println(num);
+            for(int i=0;i<num;i++){
+                System.out.println(strArray[i]);
+            }
+        }
+        scan.close();
     }
-    
-      
+}
 
